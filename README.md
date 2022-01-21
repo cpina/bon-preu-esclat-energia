14ebf00e15812e8fa1db66091f4d5e5779728a6b
Carles Pina i Estany <carles@pina.cat>
2022-01-21 22:36:21 +0100

    Afegeix requirements.txt i README.md

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # bon-preu-esclat-energia
 Obre el .xlsx per hores de Bon Preu Esclat Energia i simula pel pla "dia i nit" i "pla fem-ho fàcil"
+
+Cal instal·lar el openpyxl. Per exemple:
+
+```
+python3 -m venv venv
+pip3 install -r requirements.txt
+```
+
+Llavors per executar-ho:
+```
+./simula.py fitxer.xlsx
+```
+
+Per baixar el fitxer:
+
+-Entrar a: https://energia.bonpreuesclat.cat
+-Apretar a contractes
+-Apretar a detall del contracte que calgui (el meu pare hi té dos llocs)
+-Baixar abaix on posa "Consums" i canviar la Visualització a "Horari"
+-Posar les dates que interessin
+-Clicar a "Descarregar corba"
+
--- simula.py
+++ simula.py
@@ -1,49 +1,54 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 from datetime import datetime
 
 import openpyxl
 
-acumulat_energia = {'baix': 0,
-                    'mitja': 0,
-                    'car': 0
-                    }
+acumulats = {0.079990:0, 0.129990:0,0.229990:0 }
+
+def calcular_fesho_facil(data, hora, activa_entrant):
+    return activa_entrant * 0.179990 / 1000
 
 
 def calcular_preu_dia_i_nit(data, hora, activa_entrant):
-    global acumulat_energia
+    global acumulats
+
     hora = int(hora)
 
-    baix = 0.079990
-    mitja = 0.129990
-    car = 0.229990
+    vall = 0.079990
+    plana = 0.129990
+    punta = 0.229990
 
-    preus = {1: baix, 2: baix, 3: baix, 4: baix, 5: baix, 6: baix, 7: baix, 8: mitja, 9: mitja, 10: car, 11: car,
-             12: car, 13: car, 14: mitja, 15: mitja, 16: mitja, 17: mitja, 18: car, 19: car, 20: car, 21: car, 22: baix,
-             23: baix, 24: baix
+    preus = {1: vall, 2: vall, 3: vall, 4: vall, 5: vall, 6: vall, 7: vall, 8: vall, 9: plana, 10: plana, 11: punta,
+             12: punta, 13: punta, 14: punta, 15: plana, 16: plana, 17: plana, 18: plana, 19: punta, 20: punta, 21: punta, 22: punta,
+             23: plana, 24: plana
              }
 
     dt = datetime.strptime(data, "%d/%m/%Y")
     dia_de_la_setmana = dt.weekday()
 
-    if data == "x06/12/2021" or data == "x08/12/2021":
-        preu = baix
+    if data == "06/12/2021" or data == "08/12/2021":
+        preu = vall
     elif dia_de_la_setmana == 5 or dia_de_la_setmana == 6:
-        preu = baix
+        preu = vall
     else:
         preu = preus[hora]
+
+    acumulats[preu] += activa_entrant
 
     return preu * activa_entrant / 1000
 
 
-def simula_dia_i_nit(fitxer):
+def calcula(fitxer, calcula_preu_func):
     workbook = openpyxl.load_workbook(fitxer)
     sheet = workbook.get_active_sheet()
 
     row = 2
 
     preu_acumulat = 0
+    activa_entrant_acumulat = 0
 
     while True:
         cups = sheet.cell(row=row, column=1).value
@@ -53,8 +58,7 @@ def simula_dia_i_nit(fitxer):
         data = sheet.cell(row=row, column=2).value
         hora = sheet.cell(row=row, column=3).value
         activa_entrant = sheet.cell(row=row, column=4).value
-
-        preu_acumulat += calcular_preu_dia_i_nit(data, hora, activa_entrant)
+        preu_acumulat += calcula_preu_func(data, hora, activa_entrant)
 
         row += 1
 
@@ -62,9 +66,16 @@ def simula_dia_i_nit(fitxer):
 
 
 def simula(fitxer):
-    preu_dia_i_nit = simula_dia_i_nit(fitxer)
-    print(preu_dia_i_nit)
+    global acumulats
 
+    preu_dia_i_nit = calcula(fitxer, calcular_preu_dia_i_nit)
+    fesho_facil = calcula(fitxer, calcular_fesho_facil)
+    print(os.path.basename(fitxer))
+    print(f"Dia i nit   : {preu_dia_i_nit:.2f}€")
+    print(f"Fes-ho fàcil: {fesho_facil:.2f}€")
+
+    for preu, energia in acumulats.items():
+        print(preu, energia/1000)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
